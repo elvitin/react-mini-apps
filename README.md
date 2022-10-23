59513ceedc2f198c9c5914415888276efbd31286
Victor Rodrigues <victortavi@outlook.com>
2022-10-23 18:46:30 -0300

    Project Finalizado
    Projeto Finalizado

--- README.md
+++ README.md
@@ -1,70 +1,59 @@
-# Getting Started with Create React App
+# To Do Lista - Atividade Linguagens de Programação I
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Aplicação SPA, To Do List, que faz uso de estado para gerenciar tarefas.
 
-## Available Scripts
+<div align='center'>
 
-In the project directory, you can run:
+Victor Taveira - RA: 261911759
 
-### `npm start`
+| [![](https://github.com/src-rodrigues.png?size=150)](https://github.com/src-rodrigues) |
+| :------------------------------------------------------------------------------------: |
+|                  [Victor Rodrigues](https://github.com/src-rodrigues)                  |
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+</div>
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+---
 
-### `npm test`
+## Instalação e execução
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+Dentro da pasta do projeto,
 
-### `npm run build`
+### `Instale as dependências com:`
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+```bash
+$ npm install
+```
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+### `Execute a aplicação com:`
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+```bash
+$ npm start
+```
 
-### `npm run eject`
+para iniciar o aplicativo em modo desenvolvimento.\
+Com `ctrl + click` clique em [http://localhost:3000](http://localhost:3000) para visualizar no seu navegador.
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+A página vai recarregar sempre que as alterações são salvas.\
+Você também pode ver erros de lint no console.
 
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+### `Construa a aplicação com:`
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
+```bash
+$ npm run build
+```
 
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
+`npm run build` compila o aplicativo para produção gerando uma pasta `build`.
 
-## Learn More
+A compilação é minificada e os nomes dos arquivos incluem os hashes.\
+Seu aplicativo está pronto para ser implantado!
 
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+Veja a seção sobre [deployment](https://facebook.github.io/create-react-app/docs/deployment) para mais informações.
 
-To learn React, check out the [React documentation](https://reactjs.org/).
+---
 
-### Code Splitting
+## Tecnologias
 
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+- [React](https://pt-br.reactjs.org/)
+- [React Hooks](https://pt-br.reactjs.org/docs/hooks-intro.html)
+- [Bootstrap](https://getbootstrap.com/)
+- [Reactstrap](https://reactstrap.github.io/)
--- src/App.js
+++ src/App.js
@@ -1,24 +1,154 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+import {
+  Button,
+  ButtonGroup,
+  Col,
+  Container,
+  Input,
+  InputGroup,
+  InputGroupText,
+  ListGroup,
+  ListGroupItem,
+  Row,
+  Toast,
+  ToastBody,
+  ToastHeader
+} from 'reactstrap';
 
+import './App.css';
 function App() {
+  function GenUUID() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
+      /[xy]/g,
+      function (c) {
+        var r = (Math.random() * 16) | 0,
+          v = c === 'x' ? r : (r & 0x3) | 0x8;
+        return v.toString(16);
+      }
+    );
+  }
+
+  //factory function
+  function createBaseTask() {
+    return { id: null, taskMsg: '', done: false };
+  }
+  function createBaseOperationMsgState() {
+    return { operationMsg: '' };
+  }
+
+  const [taskItemState, setTaskItemState] = useState(createBaseTask());
+
+  const [operationMsgState, setOperationMsgState] = useState(
+    createBaseOperationMsgState()
+  );
+
+  const [taskListState, setTaskListState] = useState([]);
+
+  function refreshTaskListState() {
+    setTaskListState([...taskListState]);
+  }
+
+  function showOperationMsg(operationMsg, timeMilliseconds = 2000) {
+    setOperationMsgState({ operationMsg });
+    setTimeout(_ => {
+      setOperationMsgState(createBaseOperationMsgState());
+    }, timeMilliseconds);
+  }
+
+  function add() {
+    if (taskItemState.taskMsg === '') {
+      showOperationMsg('A mensagem da tarefa não pode ser vazia');
+      return;
+    }
+
+    if (taskItemState.id !== null) {
+      //update
+      taskListState.at(taskItemState.indexTaskToUpdate).taskMsg =
+        taskItemState.taskMsg;
+
+      setTaskItemState(createBaseTask());
+      refreshTaskListState();
+      showOperationMsg('Tarefa atualizada');
+    } else {
+      //add
+      const task = { ...taskItemState, id: GenUUID() };
+      taskListState.push(task);
+      setTaskItemState(createBaseTask());
+      refreshTaskListState();
+      showOperationMsg('Tarefa adicionada');
+    }
+  }
+
+  function setDone(item) {
+    item.done = !item.done;
+    refreshTaskListState();
+  }
+
+  function remove(index) {
+    if(taskItemState.id === taskListState[index].id){
+      setTaskItemState(createBaseTask());
+    }
+    taskListState.splice(index, 1);
+
+    refreshTaskListState();
+    showOperationMsg('Tarefa removida');
+  }
+
+  function edit(item) {
+    setTaskItemState(item);
+  }
+
+  function changeTaskMsg(value) {
+    setTaskItemState({ ...taskItemState, taskMsg: value });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container >
+        <Row className='mt-5 mb-3'>
+          <Col>
+            <h1 className='text-center'>To Do Lista - Atividade Linguagens de Programação I</h1>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col className='d-flex mb-3'>
+            <InputGroup>
+              <InputGroupText>Digite a tarefa aqui</InputGroupText>
+              <Input type='text' value={taskItemState.taskMsg} onChange={e => changeTaskMsg(e.target.value)}/>
+            </InputGroup>
+            <Button color='primary' onClick={add} style={{marginLeft: '0.8em'}}>Adicionar</Button>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <ListGroup>
+              {taskListState.map((item, index) => (
+                <ListGroupItem key={item.id}>
+                  <div className='d-flex'>
+                    <div className='d-flex align-items-center' style={{ width: '100%' }}>
+                      <Input id={item.id} type='checkbox' checked={item.done} onChange={_ => setDone(item)} style={{ marginRight: '0.8em' }}/>
+                      <label htmlFor={item.id} style={{textDecoration: item.done ? 'line-through' : 'none'}}>{item.taskMsg}</label>
+                    </div>
+                    <ButtonGroup>
+                      <Button color='warning' onClick={_ => edit(item)}>Editar</Button>
+                      <Button color='danger' onClick={_ => remove(index)}>Remover</Button>
+                    </ButtonGroup>
+                  </div>
+                </ListGroupItem>
+              ))}
+            </ListGroup>
+          </Col>
+        </Row>
+      </Container>
+
+      <Toast isOpen={ operationMsgState.operationMsg !== '' } style={{ position: 'absolute', bottom: 30, right: 30 }} >
+        <ToastHeader icon='success'>Atenção</ToastHeader>
+        <ToastBody>{operationMsgState.operationMsg}</ToastBody>
+      </Toast>
+    </>
   );
 }
 
